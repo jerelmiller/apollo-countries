ded902c97870326b2ad3eb489d32cd7a34ee21c7
Jerel Miller <jerelmiller@gmail.com>
2022-08-14 23:48:09 -0600

    Add some helpful context for useDeepMemo

--- src/hooks/useDeepMemo.ts
+++ src/hooks/useDeepMemo.ts
@@ -2,6 +2,16 @@ import { useMemo, useRef } from 'react';
 import isEqual from '@wry/equality';
 import type { DependencyList } from 'react';
 
+// This hook is useful to provide more natural interfaces for other hooks.
+//
+// For example, the `useQuery` hook takes an `options` argument which includes
+// GraphQL variables. GraphQL `variables` are defined as objects in the request
+// body to the GraphQL server. Its important to memoize the `variables` argument
+// because it helps us to determine whether we need to rerun the GraphQL query
+// when variables change. Rather than forcing the consumer of the hook to
+// remember to memoize `variables`, we can provide a nicer API by allowing an
+// object to be initialized each time.
+
 const useDeepMemo = <T>(factory: () => T, deps: DependencyList) => {
   const ref = useRef(deps);
 
