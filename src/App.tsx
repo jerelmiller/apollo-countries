65d92d3f6b884b5dd550a1799bffb8d66ee101db
Jerel Miller <jerelmiller@gmail.com>
2022-08-14 22:40:33 -0600

    Implement rough fetch of countries

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
 import './App.scss';
+import useQuery from './hooks/useQuery';
+import graphql from './graphql';
+
+const QUERY = graphql`
+  query CountriesQuery {
+    countries {
+      name
+      code
+    }
+  }
+`;
+
+interface CountriesQuery {
+  countries: { name: string; code: string }[];
+}
 
 const App = () => {
-  return <div>Countries</div>;
+  const { data, error, status } = useQuery<CountriesQuery>(QUERY);
+
+  if (status === useQuery.STATUS.LOADING) {
+    return <div>Loading</div>;
+  }
+
+  if (status === useQuery.STATUS.ERROR) {
+    return <div>{error.message}</div>;
+  }
+
+  return (
+    <ul>
+      {data.countries.map((country) => (
+        <li key={country.code}>
+          {country.code} - {country.name}
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default App;
