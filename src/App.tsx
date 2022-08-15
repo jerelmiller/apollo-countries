9be4f3f3d6b6395ab6d407ea5353ae18bd8bbebe
Jerel Miller <jerelmiller@gmail.com>
2022-08-14 23:02:46 -0600

    Add logo and some basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import './App.scss';
 import Countries from './components/Countries';
+import Logo from './components/Logo';
 
 const App = () => {
   return (
     <main className="App">
+      <header className="App-header">
+        <Logo className="App-logo" />
+        <h1>Jerel's GraphQL Countries</h1>
+      </header>
       <Countries />
     </main>
   );
